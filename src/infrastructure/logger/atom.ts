3ecd7bf5b103a5be977d1225af4cc276422637f1
devgiljong <calf-love@naver.com>
2024-02-05 03:03:23 +0900

    :sparkles: Event

--- src/infrastructure/logger/atom.ts
+++ src/infrastructure/logger/atom.ts
@@ -1,2 +1,9 @@
-export type FEATURE = 'post' | 'resume' | 'main';
+export type FEATURE = 'blog';
 
+export type PAGE = '/resume' | `/post/${string}` | '/';
+
+export type AT = 'header' | 'footer' | 'post' | 'post-card' | 'side-bar';
+
+export type TARGET = '';
+
+export type ACTION = 'click' | 'page';
