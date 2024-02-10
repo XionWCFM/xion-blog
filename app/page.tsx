ceccd03702be923541477d54d7af3156cf4541f6
devgiljong <calf-love@naver.com>
2024-02-11 00:12:28 +0900

    :sparkles: Gfm 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { allPosts, Post } from 'contentlayer/generated';
 import { PostCard, getAllPost } from '@/src/entities/post';
 import { Input } from '@/src/shared/common-ui/input';
+import { Spacing } from '@/src/shared/common-ui/spacing';
 
 export default function Home() {
   const posts = getAllPost(allPosts);
 
   return (
     <main className="">
-      <form className=" max-w-[768px]">
+      <form className=" w-full">
         <Input className=" w-full" />
       </form>
-      
+      <Spacing className=" mt-64" />
       {posts.map((post, idx) => (
         <PostCard key={idx} post={post} />
       ))}
