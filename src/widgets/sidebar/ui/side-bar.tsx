dcfdbfcfc4112aca3b0b6f1d8a12b675e16c905c
devgiljong <calf-love@naver.com>
2024-02-12 00:41:54 +0900

    :sparkles: Add

--- src/widgets/sidebar/ui/side-bar.tsx
+++ src/widgets/sidebar/ui/side-bar.tsx
@@ -5,8 +5,6 @@ import { useSideBar } from '@/src/shared/store/side-bar-store';
 export const SideBar = () => {
   const { isSideBarOpen } = useSideBar();
   return isSideBarOpen ? (
-    <nav className=" flex flex-col fixed-top-right bg-primary-400 ">
-      hello world
-    </nav>
+    <nav className=" flex flex-col fixed-top-right bg-primary-400 "></nav>
   ) : null;
 };
