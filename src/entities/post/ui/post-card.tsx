c741c7d710c81004d911507cbee4bfd166f1d221
devgiljong <calf-love@naver.com>
2024-02-12 00:19:37 +0900

    :sparkles: Add

--- src/entities/post/ui/post-card.tsx
+++ src/entities/post/ui/post-card.tsx
@@ -1,5 +1,6 @@
+import { Spacing } from '@/src/shared/common-ui/spacing';
+import { dateConverter } from '@/src/shared/util/date-converter';
 import { Post } from 'contentlayer/generated';
-import { format, parseISO } from 'date-fns';
 import Link from 'next/link';
 
 interface PostCardProps {
@@ -8,17 +9,26 @@ interface PostCardProps {
 
 export const PostCard = ({ post }: PostCardProps) => {
   return (
-    <article className=" border-t border-foreground/20 py-48">
-      <h2 className="mb-1 text-xl">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-        >
-          {post.title}
-        </Link>
+    <article className=" border-t border-foreground/20 py-48 flex flex-col">
+      <div className="">
+        <span className=" text-caption-2 leading-caption-2 font-caption-2 text-primary-400 px-8 py-4 bg-primary-100 rounded-md">
+          {post.categories}
+        </span>
+      </div>
+      <Spacing className=" mt-12" />
+      <h2 className=" text-sub-title-4 font-sub-title-4 leading-sub-title-4 text-foreground/70">
+        <Link href={post.url}>{post.title}</Link>
       </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), 'yyyy.MM.dd')}
+      <Spacing className=" mt-2" />
+      <span className=" text-caption-1 font-caption-1 leading-caption-1 text-foreground/50">
+        {post.description}
+      </span>
+      <Spacing className=" mt-16" />
+      <time
+        dateTime={post.date}
+        className=" text-caption-2 leading-caption-2 font-caption-2 text-foreground/30"
+      >
+        {dateConverter(post.date, 'yyyy.MM.dd')}
       </time>
     </article>
   );
