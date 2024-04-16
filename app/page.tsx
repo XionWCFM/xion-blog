9c7a135fd35cfdeef32f2eaa42e457de8188be65
devgiljong <calf-love@naver.com>
2024-04-17 02:08:26 +0900

    :construction: 프론트매터로 변경후 map

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,13 @@ import { getAllPosts } from "~/entities/post";
 export default async function Home() {
   const posts = await getAllPosts();
 
-  return <main className="">{}</main>;
+  return (
+    <main className=" flex justify-center items-center flex-col">
+      {posts.map((post) => (
+        <div className="" key={post.title}>
+          {post.title}
+        </div>
+      ))}
+    </main>
+  );
 }
--- src/widgets/layout/ui/layout.tsx
+++ src/widgets/layout/ui/layout.tsx
@@ -7,7 +7,7 @@ interface indexProps extends React.PropsWithChildren {}
 
 const Layout = ({ children }: indexProps) => {
   return (
-    <div className="px-16 md:px-0 w-screen flex flex-col justify-center items-center">
+    <div className=" px-16 md:px-0 w-screen flex flex-col justify-center items-center">
       <div className=" px-0  w-full md:w-[768px] lg:w-1024 ">
         <Header />
       </div>
