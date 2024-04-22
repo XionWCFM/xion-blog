29da3bc87a32d9da6c37a9b004850539a9beda0c
devgiljong <calf-love@naver.com>
2024-04-23 00:39:26 +0900

    :construction: Pagenation 구현 및 테스팅

--- src/shared/hook/use-pagenation.test.ts
+++ src/shared/hook/use-pagenation.test.ts
@@ -1,12 +1,27 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 
 import { usePagenation } from "./use-pagenation";
 
-describe("", () => {
-  it("", () => {
-    const { result } = renderHook(() => usePagenation(["hello", "world"]));
-    const value = result.current;
-    console.log(value);
-    expect(true).toBe(true);
+describe("use-pagenation 훅의 동작을 테스트합니다.", () => {
+  const getArray = (len: number) => Array.from(new Array(len));
+  it("기본값을 테스트합니다.", () => {
+    const { result } = renderHook(() => usePagenation(getArray(500)));
+    expect(result.current).toMatchObject({
+      firstNum: 1,
+      lastNum: 10,
+      startPageNum: 1,
+      totalPageLength: 26,
+      pagenationButtonList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      listSize: 20,
+      activePage: 1,
+      buttonCount: 10,
+      maxLength: 500
+    });
+  });
+  it("back 함수는 현재 페이지가 1일때 back을 수행해도 1이어야합니다.", async () => {
+    const { result } = renderHook(() => usePagenation(getArray(20)));
+    act(() => result.current.back());
+    console.log(result.current);
+    expect(result.current.activePage).toBe(1);
   });
 });
--- src/shared/hook/use-pagenation.ts
+++ src/shared/hook/use-pagenation.ts
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 type PagenationConfigType = {
+  value?: number;
+  onValueChange?: (num: number | ((num: number) => void)) => void;
   listSize?: number;
   initialActivePage?: number;
   buttonCount?: number;
@@ -15,14 +17,54 @@ export const usePagenation = <List extends Array<unknown>>(
   list: List,
   config?: PagenationConfigType
 ) => {
-  const [listSize, setListSize] = useState(config?.listSize ?? DEFAULT_LIST_SIZE);
-  const [activePage, setActivePage] = useState(config?.initialActivePage ?? DEFAULT_ACTIVE_PAGE);
-  const [buttonCount, setButtonCount] = useState(config?.buttonCount ?? DEFAULT_BUTTON_COUNT);
-  const [maxLength, setMaxLength] = useState(config?.maxLength ?? list.length);
+  const [localActivePage, setLocalActivePage] = useState(
+    config?.initialActivePage ?? DEFAULT_ACTIVE_PAGE
+  );
+
+  const activePage = config?.value ?? localActivePage;
+  const setActivePage = config?.onValueChange ?? setLocalActivePage;
+  const listSize = config?.listSize ?? DEFAULT_LIST_SIZE;
+  const buttonCount = config?.buttonCount ?? DEFAULT_BUTTON_COUNT;
+  const maxLength = config?.maxLength ?? list.length;
+
+  const startPageNum = 1;
+  const totalPageLength = Math.round(maxLength / listSize) + 1;
+
   const firstNum = activePage - (activePage % listSize) + 1;
-  const lastNum = activePage - (activePage % listSize) + buttonCount;
+  const lastNum = Math.min(activePage - (activePage % listSize) + buttonCount, totalPageLength);
+
+  const pagenationButtonList = Array.from(new Array(lastNum - firstNum + 1)).map(
+    (_, idx) => firstNum + idx
+  );
+
+  const next = (nextStep = 1) => {
+    setActivePage((prev) => {
+      return Math.min(prev + nextStep, totalPageLength);
+    });
+  };
+  const back = (backStep = -1) => {
+    setActivePage((prev) => {
+      return Math.max(prev + backStep, 1);
+    });
+  };
+  const select = (step: number) => {
+    if (step === activePage) return;
+    if (step > lastNum) return;
+    if (step < firstNum) return;
+    return setActivePage(step);
+  };
   return {
     firstNum,
-    lastNum
+    lastNum,
+    startPageNum,
+    totalPageLength,
+    pagenationButtonList,
+    listSize,
+    activePage,
+    buttonCount,
+    maxLength,
+    next,
+    back,
+    select
   };
 };
